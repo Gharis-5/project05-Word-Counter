2e30a28cc490306fa2f782382765a24c93fc2e87
Gharis Khalil <gharissheikh678@gmail.com>
2024-07-11 11:53:37 +0500

    word counter
    word counter

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 #! /usr/bin/env node
-
 import inquirer from "inquirer";
-
-const answers: { Sentence: string } = await inquirer.prompt([
-  {
-    name: "Sentence",
-    type: "input",
-    message: "Enter Your Sentence To Count The Word : ",
-  },
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter Your Sentence To Count The Word : ",
+    },
 ]);
-
 const word = answers.Sentence.trim().split(" ");
-
 console.log(word);
-
 console.log(`Your Sentence Word Count Is ${word.length}`);
